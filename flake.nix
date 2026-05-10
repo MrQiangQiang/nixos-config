2d516605616f9f0de824776c579a65f0415f15d3
a <chenzhiqiang0125@gmail,com>
2026-05-10 08:31:40 +0000

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # disko = {
+    #  url = "github:nix-community/disko";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
