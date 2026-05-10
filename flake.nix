da18d5279ba8be556c07a8b80bec61f89e4d6992
a <chenzhiqiang0125@gmail,com>
2026-05-10 16:32:55 +0000

    update flake

--- flake.nix
+++ flake.nix
@@ -19,7 +19,10 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
-      import = [ ./modules/river-kwm.nix ];
+      imports = [ 
+	./modules/river-kwm.nix
+        inputs.home-manager.flakeModule 
+      ];
 
       perSystem = { config, pkgs, ... }: {
         _module.args.pkgs = pkgs;
