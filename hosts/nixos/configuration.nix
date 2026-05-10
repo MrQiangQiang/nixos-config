6fe531cb03917acb9f4a4cb98f411d2e49768c7e
a <chenzhiqiang0125@gmail,com>
2026-05-10 06:31:36 +0000

    fix

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -16,7 +16,6 @@
       "https://mirrors.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDshjY="
