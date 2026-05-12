eb63c7736c31e85ab8e77c6086241f6c1b272030
a <chenzhiqiang0125@gmail,com>
2026-05-12 18:50:32 +0000

    fix kwm

--- packages/kwm.nix
+++ packages/kwm.nix
@@ -2,7 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  zig,
+  zig_0_15,
   pkg-config,
   wayland,
   libxkbcommon,
@@ -22,9 +22,10 @@ let
     hash = "sha256-UIBuOC+kAMQmKBwqmefUvz9PhSDb/TNWcNh5CxItnc8=";
   };
   
-  zigDeps = zig.fetchDeps {
+  zigDeps = zig_0_15.fetchDeps {
     inherit pname version src;
-    hash = "sha256-50jjjYCr6EkRrAfkL8u4CZii7jjUahM94ka3m2cCfM0=";
+    hash = "";
+#    hash = "sha256-50jjjYCr6EkRrAfkL8u4CZii7jjUahM94ka3m2cCfM0=";
   };
 
 in
@@ -39,7 +40,8 @@ stdenv.mkDerivation {
   strictDeps = true;
  
   nativeBuildInputs = [
-    zig.hook
+    zig_0_15.hook
+    zig_0_15
     pkg-config
     wayland-protocols
     git
@@ -52,11 +54,26 @@ stdenv.mkDerivation {
     fcft
   ];
   
+#  zigBuildFlags = [
+#    "--system" "${zigDeps}" 
+#  ];
+  
+#  preBuild = ''
+#    export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+#    mkdir -p $ZIG_GLOBAL_CACHE_DIR/p
+#    if [ -d ${zigDeps}/p ]; then
+#      cp -a ${zigDeps}/p/. $ZIG_GLOBAL_CACHE_DIR/p/
+#    else
+#      cp -a ${zigDeps}/. $ZIG_GLOBAL_CACHE_DIR/p/
+#    fi
+#    chmod -R +w $ZIG_GLOBAL_CACHE_DIR
+#  '';
+  
   preBuild = ''
     export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
-    mkdir -p $ZIG_GLOBAL_CACHE_DIR
-    cp -a ${zigDeps}/. $ZIG_GLOBAL_CACHE_DIR/
-    chmod -R +w $ZIG_GLOBAL_CACHE_DIR
+    mkdir -p $ZIG_GLOBAL_CACHE_DIR/p
+    cp -a ${zigDeps}/. $ZIG_GLOBAL_CACHE_DIR/p/
+    chmod -R +rw "$ZIG_GLOBAL_CACHE_DIR"
   '';
 
   meta = with lib; {
