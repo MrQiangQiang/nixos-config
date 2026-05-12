7f69de5c08b1388e97f5d119052a0f73b5d48045
a <chenzhiqiang0125@gmail,com>
2026-05-12 13:56:23 +0000

    fix: kwm

--- packages/kwm.nix
+++ packages/kwm.nix
@@ -22,7 +22,7 @@ let
     hash = "sha256-UIBuOC+kAMQmKBwqmefUvz9PhSDb/TNWcNh5CxItnc8=";
   };
   
-  zigDeps = zig.fetchDeps = {
+  zigDeps = zig.fetchDeps {
     inherit pname version src;
     hash = "sha256-50jjjYCr6EkRrAfkL8u4CZii7jjUahM94ka3m2cCfM0=";
   };
