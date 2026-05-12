b813ec695123c608827a7efc5df0defba2542e7a
a <chenzhiqiang0125@gmail,com>
2026-05-12 07:25:54 +0000

    fix

--- packages/kwm.nix
+++ packages/kwm.nix
@@ -55,6 +55,6 @@ stdenv.mkDerivation {
     description = "Window manager for River Wayland cpmpositor (zig)";
     homepage = "https://github.com/kewuaa/kwm";
     license = licenses.gpl3;
-    platforms = platforms.linus;   
+    platforms = platforms.linux;   
   };
 }
