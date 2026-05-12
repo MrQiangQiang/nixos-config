8b484efeba27e217a8c100408ba5e5af5c67e5e7
a <chenzhiqiang0125@gmail,com>
2026-05-12 13:55:08 +0000

    kwm deps cache

--- packages/kwm.nix
+++ packages/kwm.nix
@@ -21,14 +21,20 @@ let
     rev = "d3d6ec2c13c830f312a79c8cb7b908964ecb5c84"; 
     hash = "sha256-UIBuOC+kAMQmKBwqmefUvz9PhSDb/TNWcNh5CxItnc8=";
   };
+  
+  zigDeps = zig.fetchDeps = {
+    inherit pname version src;
+    hash = "sha256-50jjjYCr6EkRrAfkL8u4CZii7jjUahM94ka3m2cCfM0=";
+  };
+
 in
 stdenv.mkDerivation {
   inherit pname version src;
  
-  zigCachePrefix = zig.fetchDeps {
-    inherit pname version src;
-    hash = "sha256-50jjjYCr6EkRrAfkL8u4CZii7jjUahM94ka3m2cCfM0=";
-  };
+ # zigCachePrefix = zig.fetchDeps {
+ #   inherit pname version src;
+ #   hash = "sha256-50jjjYCr6EkRrAfkL8u4CZii7jjUahM94ka3m2cCfM0=";
+ # };
 
   strictDeps = true;
  
@@ -45,6 +51,13 @@ stdenv.mkDerivation {
     pixman
     fcft
   ];
+  
+  preBuild = ''
+    export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+    mkdir -p $ZIG_GLOBAL_CACHE_DIR
+    cp -a ${zigDeps}/. $ZIG_GLOBAL_CACHE_DIR
+    chmod -R +w $ZIG_GLOBAL_CACHE_DIR
+  '';
 
   meta = with lib; {
     description = "Window manager for River Wayland cpmpositor (zig)";
