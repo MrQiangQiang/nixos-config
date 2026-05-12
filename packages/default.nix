be66c30d9bd7174179b7d891fe012cb340bbdd8c
a <chenzhiqiang0125@gmail,com>
2026-05-12 12:28:34 +0000

    fix

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  perSystem = { pkgs, ... }: {
+  perSystem = { pkgs, inputs, ... }: {
     packages.kwm = pkgs.callPackage ./kwm.nix { zig-overlay = inputs.zig-overlay; };
   };
 }
