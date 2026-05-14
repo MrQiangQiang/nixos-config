9ff939926beedb85bbb60f4b233e92faf1e211dd
a <chenzhiqiang0125@gmail,com>
2026-05-14 17:54:46 +0000

    kwm

--- packages/kwm.nix
+++ packages/kwm.nix
@@ -9,7 +9,8 @@
   wayland-protocols,
   libxkbcommon,
   pixman,
-  fcft
+  fcft,
+  libevdev
 }:
 
 let
@@ -19,14 +20,15 @@ let
   src = fetchFromGitHub {
     owner = "kewuaa";
     repo = "kwm";
-    rev = "d3d6ec2c13c830f312a79c8cb7b908964ecb5c84"; 
+    rev = "d3d6ec2c13c830f312a79c8cb7b908964ecb5c84";
     hash = "sha256-UIBuOC+kAMQmKBwqmefUvz9PhSDb/TNWcNh5CxItnc8=";
   }; 
 
   zigDeps = zig_0_15.fetchDeps {
     inherit pname version src;
-    hash = "";
+#    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; 
 #    hash = "sha256-RxaOdKCDduRBGMb1bb5cYgQ6WAfIG9tpuxiVhOmaEvE=";
+    hash = lib.fakeHash;
   };
 in
 stdenv.mkDerivation {
@@ -34,14 +36,16 @@ stdenv.mkDerivation {
 
   strictDeps = true;
 
-  depsBuildBuild = [
-    pkg-config
-    wayland-scanner
-  ];
+#  depsBuildBuild = [
+#    pkg-config
+#    wayland-scanner
+#  ];
 
   nativeBuildInputs = [
     zig_0_15.hook
     wayland-protocols
+    wayland-scanner
+    pkg-config 
   ];
 
   buildInputs = [
@@ -49,15 +53,35 @@ stdenv.mkDerivation {
     libxkbcommon
     pixman
     fcft
+    libevdev
   ];
 
   postConfigure = ''
-    ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
-    export ZIG_GLOBAL_CACHE_DIR
-    mkdir -p $ZIG_GLOBAL_CACHE_DIR/p
-    cp -r ${zigDeps}/* "$ZIG_GLOBAL_CACHE_DIR/p"
+    export PKG_CONFIG_PATH="${
+      lib.makeSearchPath "lib/pkgconfig" [
+        wayland-scanner
+        wayland
+        libxkbcommon
+        libevdev
+        pixman
+        fcft
+      ]
+    }:${wayland-protocols}/share/pkgconfig''${PKG_CONFIG_PATH:+:}$PKG_CONFIG_PATH"
+    export ZIG_GLOBAL_CHACHE_DIR=$TMPDIR/zig-cache
+    mkdir -p "$ZIG_GLOBAL_CACHE_DIR"
+    cp -rn ${zigDeps}/* "$ZIG_GLOBAL_CACHE_DIR/"
     chmod -R u+w "$ZIG_GLOBAL_CACHE_DIR"
+#    export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+#    mkdir -p $ZIG_GLOBAL_CACHE_DIR/p
+#    cp -r ${zigDeps}/* "$ZIG_GLOBAL_CACHE_DIR/p"
+#    chmod -R u+w "$ZIG_GLOBAL_CACHE_DIR"
   '';
+  
+#  preBuild = ''
+#    if [ -n "$deps" ]; then
+#      zigBuildFlagsArray+=("--system" "$deps")
+#    fi
+#  '';
 
   zigBuildFlags = [
     "-Doptimize=ReleaseSafe"
