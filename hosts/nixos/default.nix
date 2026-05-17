82aec4cec207b4061a73b5e937c6f4a80a42ab7d
a <chenzhiqiang0125@gmail,com>
2026-05-18 05:14:38 +0800

    fix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -5,22 +5,6 @@
   ...
 }:
 
-let
-  mirrorConfigText = ''
-    [[registry]]
-    prefix = "docker.io"
-    location = "docker.io"
-    
-    [[registry.mirror]]
-    location = "https://docker.1ms.run"
-
-    [[registry.mirror]]
-    location = "https://docker.m.daocloud.io"
-
-    [[registru.mirror]]
-    location = "https://docker.xuanyuan.me"
-  '';
-in
 {
   imports = [
     ../../modules/river.nix
@@ -51,11 +35,20 @@ in
       enable = true;
       containersConf.settings = {
         containers.unqualified-search-registries = [ "docker.io" ];
+        registry = [
+          {
+            prefix = "docker.io";
+            location = "docker.io";
+            mirror = [
+              { location = "https://docker.1ms.run"; }
+              { location = "https://docker.m.daocloud.io"; }
+              { location = "https://docker.xuanyuan.me"; }
+            ];
+          }
+        ]; 
       };
     };
   };
-
-  environment.etc."containers/registries.conf".text = mirrorConfigText;
 
   environment.systemPackages = with pkgs; [
     distrobox
