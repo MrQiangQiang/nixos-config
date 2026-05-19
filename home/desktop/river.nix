034f3142b8d7f9c27cf1d26737391d87e06f7796
a <chenzhiqiang0125@gmail,com>
2026-05-20 03:41:43 +0800

    fix

--- home/desktop/river.nix
+++ home/desktop/river.nix
@@ -13,10 +13,16 @@ lib.mkIf isRiverEnabled {
   wayland.windowManager.river = {
     enable = true;
     systemd.enable = true;
-    spawn = [
-      "swaybg -m color -c '#FF0000'"
-      "waybar"
-    ];
-    run = "kwm";
+  };
+  
+  xdg.configFile."river/init" = {
+    executable = true;
+    text = ''
+      #!/bin/sh
+      swaybg -m color -c "#FF0000" &
+      waybar &
+      
+      exec kwm
+    '';
   }; 
 }
