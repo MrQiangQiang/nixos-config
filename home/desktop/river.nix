e891898d1a443a21f165a950437481ba7817c14c
a <chenzhiqiang0125@gmail,com>
2026-05-20 03:31:46 +0800

    fix

--- home/desktop/river.nix
+++ home/desktop/river.nix
@@ -9,19 +9,14 @@
 let
   isRiverEnabled = osConfig.custom.river.enable or false;
 in
-lib.mkIf isRiverEnabled { 
-  xdg.configFile."river/init" = {
-    executable = true;
-    text = ''
-      #!/bin/sh
-      
-      export XDG_CURRENT_DESKTOP=river
-      export XDG_SESSION_TYPE=wayland
-   
-      swaybg -m color -c "#FF0000" &
-      waybar &
-      
-      exec kwm
-    '';
+lib.mkIf isRiverEnabled {
+  wayland.windowManager.river = {
+    enable = true;
+    systemd.enable = true;
+    spawn = [
+      "swaybg -m color -c '#FF0000'"
+      "waybar"
+    ];
+    run = "kwm";
   }; 
 }
--- modules/river.nix
+++ modules/river.nix
@@ -28,12 +28,24 @@ in
       wlr.enable = true;
       extraPortals = [
         pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-wlr
       ];
       config = {
         common.default = "*";
         river.default = [ "wlr" "gtk" ];
       };
     };
+    
+    environment.etc."river/init".text = ''
+      #!/bin/sh
+      systemctl --user import-environment WAYLAND_DISPLAY 
+XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY 
+XDG_CURRENT_DESKTOP=river XDG_SESSION_TYPE=wayland
+
+      systemctl --user restart xdg-desktop-portal.service || true
+      systemctl --user restart xdg-desktop-portal-gtk.service || true
+    '';
   };
 }
 
