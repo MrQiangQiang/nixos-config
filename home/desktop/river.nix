523bac876a4374b9c06ab067c06f07350bc8c719
a <chenzhiqiang0125@gmail,com>
2026-05-20 04:17:05 +0800

    fix

--- home/desktop/river.nix
+++ home/desktop/river.nix
@@ -10,18 +10,24 @@ let
   isRiverEnabled = osConfig.custom.river.enable or false;
 in
 lib.mkIf isRiverEnabled {
-  wayland.windowManager.river = {
-    enable = true;
-    systemd.enable = true;
-  };
-  
   xdg.configFile."river/init" = {
     executable = true;
     text = ''
       #!/bin/sh
+      export XDG_CURRENT_DESKTOP=river
+      export XDG_SESSION_TYPE=wayland
+
       swaybg -m color -c "#FF0000" &
       waybar &
-      
+
+      systemctl --user import-environment WAYLAND_DISPLAY 
+XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY 
+XDG_CUREENT_DESKTOP=river XDG_SESSION_TYPE=wayland
+
+      systemctl --user restart xdg-desktop-portal.service || true
+      systemctl --user restart xdg-desktop-portal-gtk.service || true    
+
       exec kwm
     '';
   }; 
