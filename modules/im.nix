593e0272207e5006c18c24c7f415ac8af2a98b83
a <chenzhiqiang0125@gmail,com>
2026-05-20 19:45:03 +0800

    chore: im

--- modules/im.nix
+++ modules/im.nix
@@ -4,21 +4,11 @@
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
-      fcitx5-chinese-addons
+      qt6Packages.fcitx5-chinese-addons
       fcitx5-gtk
-      fcitx5-qt
+      qt6Packages.fcitx5-qt
     ];
   };
-
-  environment.variables = with pkgs; {
-    GTK_IM_MODULE = lib.mkForce "fcitx5";
-    QT_IM_MODULE = lib.mkForce "fcitx5";
-    XMODIFIERS = lib.mkForce "@im=fcitx";
-  };
-
-  environment.systemPackages = with pkgs; [
-    fcitx5-configtool
-  ];
 
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
 }
