9ae8a711c6c598a576a717d0bf3bcc61b290d7a1
a <chenzhiqiang0125@gmail,com>
2026-05-23 20:28:19 +0800

    fix

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -64,13 +64,13 @@ let
       # 强制 Trae 与字节跳动相关流量代理
       - DOMAIN-SUFFIX,trae-cn,全自动最优节点
       - DOMAIN-SUFFIX,trae.com.cn,全自动最优节点
-      - DOMAIN-SUFFIX,bytedance.com,全自动最优节
-      - DOMAIN-SUFFIX,bytedance.net,全自动最优节
-      - DOMAIN-SUFFIX,byted.org,全自动最优节
-      - DOMAIN-SUFFIX,zijieapi.com,全自动最优节
-      - DOMAIN-SUFFIX,mchost.gurn,全自动最优节
-      - DOMAIN-SUFFIX,bytedanceapi.com,全自动最优节
-      - DOMAIN-SUFFIX,volcengine.com,全自动最优节
+      - DOMAIN-SUFFIX,bytedance.com,全自动最优节点
+      - DOMAIN-SUFFIX,bytedance.net,全自动最优节点
+      - DOMAIN-SUFFIX,byted.org,全自动最优节点
+      - DOMAIN-SUFFIX,zijieapi.com,全自动最优节点
+      - DOMAIN-SUFFIX,mchost.gurn,全自动最优节点
+      - DOMAIN-SUFFIX,bytedanceapi.com,全自动最优节点
+      - DOMAIN-SUFFIX,volcengine.com,全自动最优节点
 
       # 1. 优先进行【域名】匹配（秒级命中，不触发后台 DNS 等待）
       - GEOSITE,private,DIRECT
