513720876f4ca0d4876be2ee6688e2e99a04f68e
a <chenzhiqiang0125@gmail,com>
2026-06-01 16:39:01 +0800

    feat: add SSH signing, known hosts, and shell tools
    - ssh.nix: enable ssh-agent, add GitHub/GitLab/Codeberg known hosts
    - shell/default.nix: SSH match blocks, git SSH signing, allowed_signers
    - Add jq to shell packages

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,15 +1,62 @@
 { pkgs, ... }:
 
+let
+  nixosSshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvALtc74c420xWoDLT6mwGO/Mf7JemicsoeFjFo87Ez fugui@nixos";
+in
 {
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks = {
+      "*" = {
+        identitiesOnly = true;
+        addKeysToAgent = "yes";
+      };
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "gitlab.com" = {
+        hostname = "gitlab.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "codeberg.org" = {
+        hostname = "codeberg.org";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
   programs.git = {
     enable = true;
+    signing = {
+      key = nixosSshPublicKey;
+      signByDefault = true;
+    };
     settings = {
       user = {
         name = "fugui";
         email = "chenzhiqiang0125@gmail.com";
       };
+      gpg = {
+        format = "ssh";
+      };
+      gpg.ssh = {
+        allowedSignersFile = "~/.ssh/allowed_signers";
+      };
+      tag = {
+        gpgsign = true;
+      };
     };
   };
+
+  home.file.".ssh/allowed_signers".text = ''
+    chenzhiqiang0125@gmail.com ${nixosSshPublicKey}
+  '';
 
   programs.vim = {
     enable = true;
@@ -19,8 +66,9 @@
     '';
   };
 
-  home.packages = with pkgs; [ 
+  home.packages = with pkgs; [
     fastfetch
     htop
+    jq
   ];
 }
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,5 +1,23 @@
 { config, pkgs, ... }:
 {
+  programs.ssh = {
+    startAgent = true;
+    knownHosts = {
+      "github.com" = {
+        hostNames = [ "[ssh.github.com]:443" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
+      "gitlab.com" = {
+        hostNames = [ "gitlab.com" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      };
+      "codeberg.org" = {
+        hostNames = [ "codeberg.org" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB";
+      };
+    };
+  };
+
   services.openssh = {
     enable = true;
     listenAddresses = [
