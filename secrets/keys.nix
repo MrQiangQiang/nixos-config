497bafecb543d63b0ac5af6146065b749d7d67bf
a <chenzhiqiang0125@gmail,com>
2026-06-15 03:54:17 +0800

    fix(secrets): add desktop-1 host key, rekey age secrets

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -1,6 +1,7 @@
 {
   hosts = {
     laptop-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfFJst3nBZ06Bft8jtQo7bISsYox3eHDeCr5uTjfsk7 root@laptop-1";
+    desktop-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYlv4fxvq7fPjWl1aA8SVpBGxAC346b2gPWhnaaNWC1 root@desktop-1";
   };
   users = {
     fugui = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvALtc74c420xWoDLT6mwGO/Mf7JemicsoeFjFo87Ez fugui@laptop-1";
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,10 +4,12 @@ in
 {
   "proxy-subscription-url.age".publicKeys = [
     keys.hosts.laptop-1
+    keys.hosts.desktop-1
     keys.users.fugui
   ];
   "opencode-go-key.age".publicKeys = [
     keys.hosts.laptop-1
+    keys.hosts.desktop-1
     keys.users.fugui
   ];
 }
